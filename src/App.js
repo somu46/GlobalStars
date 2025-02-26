99b7f2c7e767be9bcb0275ee529e20c227704ecc
Soumyajit Ray <soumyajitray2002@gmail.com>
2025-02-27 01:51:12 +0530

    `Removed unused imports and added React Router setup`

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import Navbar from './Components/Navbar/Navbar.js';
-import Footer from './Components/Footer/Footer.js';
+// import Navbar from './Components/Navbar/Navbar.js';
+// import Footer from './Components/Footer/Footer.js';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Footer/>
+    tHIS IS APP Components
     </div>
   );
 }
