a10262654621e26d33d5f0baeaef9810a4fe4673
Soumyajit Ray <101827920+somu46@users.noreply.github.com>
2025-03-07 04:27:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root from './Components/Root/root';
-import { About, Blog, Contact, Faqs, LitigationSupport, Photo, Video ,TestAnal, BoroscopeInspection, EnvironmentalP, RadiationSafety, LandRem } from './Pages';
+import { About, Blog, Contact, Faqs, LitigationSupport, Video ,TestAnal, BoroscopeInspection, EnvironmentalP, RadiationSafety, LandRem } from './Pages';
 import { Toaster } from 'react-hot-toast';
 import Gallery from './Pages/Gallery/Gallery';
  
