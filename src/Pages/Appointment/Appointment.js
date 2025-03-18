d9b9a327d677760b92957e13995f221a76c73d64
Arkajyoti Kundu <arkajyotikundu415@gmail.com>
2025-03-19 00:55:51 +0530

    `Added useRef hook and form reference to Appointment.js`

--- src/Pages/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Button from '../../Components/UiCom/Button'
+// import { useForm } from 'react-hook-form';
 
 const Appointment = () => {
+
+    const from =useRef();
+    //  const {Date,Time,FullName, MobileNumber,PurposeOfVisit}=useForm();
+
+
   return (
     <div className="min-h-screen bg-gray-50 py-8 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md mx-auto bg-white rounded-lg shadow-sm p-6">
@@ -9,7 +15,7 @@ const Appointment = () => {
         <span className='text-[#1E40AF]'>Book</span> <span className='text-[#FFD700]'> Appointment</span> 
         </h2>
         
-        <form className="space-y-6">
+        <form ref={from} className="space-y-6">
           {/* Date and Time */}
           <div className="flex flex-col md:flex-row gap-4">
             <div className="flex-1">
@@ -17,6 +23,8 @@ const Appointment = () => {
                 Date
               </label>
               <input
+              name='Data'
+             
                 type="date"
                 id="date"
                 className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
